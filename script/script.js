b79f2eeecaf4de70f5d83a017a4286a1e85872cd
taytum-twiss <twisstr@g.crookcountyschools.org>
2021-05-20 08:55:00 -0700

    changes for 5-20-21

--- script/script.js
+++ script/script.js
@@ -1,62 +1,82 @@
-console.log('I am alive')
-console.log('hello world');
-// 3. Actually Code (what code do we write) - Easy Part*/
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
+'use strict'
 
-let leftImg = document.getElementById('leftImg');
-let centerImg = document.getElementById('centerImg');
-let rightImg = document.getElementById('rightImg');
-
-  leftImg.addEventListener('click', leftimg);
-  centerImg.addEventListener('click', centerimg);
-  rightImg.addEventListener('click', rightimg);
-  
-function leftimg() {
-  console.log('The Left Image was clicked')
-}
-function centerimg() {
-  console.log('The Center Image was clicked')
-}
-function rightimg() {
-  console.log('The Right Image was clicked')
-}
-
+// This is the mini-database to keep track of everything. 
 let PRODUCTS_ARRAY = [
-  {HTMLid : 'bag', imgURL : '../Images/bag.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'bannana', imgURL : '../Images/bannana.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'bathroom', imgURL : '../Images/bathroom.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'boots', imgURL : '../Images/boots.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'breakfast', imgURL : '../Images/breakfast.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'bubblegum', imgURL : '../Images/bubblegum.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'chair', imgURL : '../Images/chair.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'cthulhu', imgURL : '../Images/cthulhu.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'dog-duck', imgURL : '../Images/dog-duck.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'dragon', imgURL : '../Images/dragon.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'pen', imgURL : '../Images/pen.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'pet-sweep', imgURL : '../Images/pet-sweep.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'scissors', imgURL : '../Images/scissors.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'shark', imgURL : '../Images/shark.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'sweep', imgURL : '../Images/sweep.png', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'tauntaun', imgURL : '../Images/tauntaun.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'unicorn', imgURL : '../Images/unicorn.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'water-can', imgURL : '../Images/water-can.jpg', totalViews : 0, totalVotes : 0},
-  {HTMLid : 'wine-glass', imgURL : '../Images/wine-glass.jpg', totalViews : 0, totalVotes : 0},
+  {HTMLid: 'bag', imgURL: './images/bag.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'banana', imgURL: './images/banana.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'bathroom', imgURL: './images/bathroom.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'boots', imgURL: './images/boots.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'breakfast', imgURL: './images/breakfast.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'bubblegum', imgURL: './images/bubblegum.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'chair', imgURL: './images/chair.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'cthulhu', imgURL: './images/cthulhu.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'dogDuck', imgURL: './images/dog-duck.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'dragon', imgURL: './images/dragon.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'pen', imgURL: './images/pen.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'petSweep', imgURL: './images/pet-sweep.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'scissors', imgURL: './images/scissors.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'shark', imgURL: './images/shark.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'sweep', imgURL: './images/sweep.png', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'tauntaun', imgURL: './images/tauntaun.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'unicorn', imgURL: './images/unicorn.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'usb', imgURL: './images/usb.gif', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'waterCan', imgURL: './images/water-can.jpg', totalViews: 0, totalVotes: 0},
+  {HTMLid: 'wineGlass', imgURL: './images/wine-glass.jpg', totalViews: 0, totalVotes: 0},
 ]
 
-function handleClick() {
-  console.log('An Image Was Clicked')
+let clicks = 0;
+
+// On startup - display 3 images from the array
+function renderImages() {
+  // this funtion will render 3 images from the array
+  // go grab the html item to attach the 3 images to
+  // 1. get the element from html that we want to attach the image to (getElementById, or querySelector)
+  // 2. create an image element (createElement)
+  // 3. set the src property of the img element to the imgURL property of the item
+  // 4. append the newly created img to the div element (appendChild)
+
+  // let img1Container = document.getElementById('img1Container');
+  // let img2Container = document.getElementById('img2Container');
+  // let img3Container = document.getElementById('img3Container');
+
+  for (let i=0; i <= 2; i++) {
+    // let imgContainer = document.getElementById('img' + i + 'Container');
+    // imagine the following code being done i times
+    let imgContainer = document.getElementById(`img${i}Container`);
+    let img = document.createElement('img');
+    imgContainer.appendChild(img);
+    img.setAttribute('src', PRODUCTS_ARRAY[i].imgURL);
+    img.setAttribute('id', PRODUCTS_ARRAY[i].HTMLid);
+
+    //whenever the image is shown, we need to add one to the total view
+  }
+
 }
 
-function RenderImages() {
- for (let i=0; i <= 2; i++) {
-   //let imgContainer = document.getElementById('img' + 1 + 'Container');
-   let imgContainer = document.getElementById(`img${i}Container`);
-   let img = document.createElement('img');
-   img.setAttribute('src', PRODUCTS_ARRAY[i].imgURL);
- }
+// Consolidate all the response functions (callbacks) into one function
+function handleClick(event) {
+  // put all the callbacks into one place
+  clicks++;
+  console.log('I was clicked, and my id is: ', event.target.id)
+  let parentId = event.target.id;
+
+  // when the parent container is clicked, how do i know which image it is ???
+  // we have to compare the id's
+  // if then else statement.. compare parentId to image id somehow.. 
 
 
 
 }
+
+(function startApp() {
+
+  for(let i=0; i < 3; i++) {
+    let imgContainer = document.getElementById(`img${i}Container`);
+    imgContainer.addEventListener('click', handleClick);
+  }
+
+  // shuffle the images - later
+  renderImages();
+
+})();
+
